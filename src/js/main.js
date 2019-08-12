e212408d0281a63ea134a231eac4ea42704232b2
Miguel Gomes <miguel.gomes@petersfisch.local>
2019-08-12 16:02:18 +0100

    completed last challenge with state and using id

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
-import { animatedScrollTo } from "./es6-scroll-to.js";
+// import { animatedScrollTo } from "./es6-scroll-to.js";
 
 class StateCta {
   constructor(number) {
     this.state = (() => {
       let newObj = {};
       for (let i = 0; i < number; i++) {
-        newObj[`textBox${i}`] = false;
+        newObj[`textBox${i ? i : ""}`] = false;
       }
 
       return { ...newObj };
@@ -15,21 +15,32 @@ class StateCta {
   stateLength = () => Object.keys(this.state).length;
 
   isOpen = () =>
-    !Object.values(this.state).reduce((total, current) => total + current);
+    Object.values(this.state).reduce((total, current) => total + current);
 
   toggleCta = textBox => {
-    if (this.isOpen() || this.state[textBox]) {
+    if (!this.isOpen() || this.state[textBox]) {
       this.state = { ...this.state, [`${textBox}`]: !this.state[`${textBox}`] };
       document.getElementById(textBox).classList.toggle("hidden");
 
       return this.state;
+    } else {
+      let resetBtn = Object.entries(this.state).find(
+        elem => elem[1] === true
+      )[0];
+
+      this.state = {
+        ...this.state,
+        [`${textBox}`]: !this.state[`${textBox}`],
+        [`${resetBtn}`]: !this.state[`${resetBtn}`]
+      };
+      document.getElementById(resetBtn).classList.toggle("hidden");
+      document.getElementById(textBox).classList.toggle("hidden");
+
+      return this.state;
     }
-    console.log("close other tooltip first");
-    console.log(this.state);
   };
 
   bindBtn = () => {
-    console.log(this.stateLength());
     for (let i = 0; i < this.stateLength(); i++) {
       document.getElementById(`btn${i ? i : ""}`).onclick = () => {
         this.toggleCta(`textBox${i ? i : ""}`);
@@ -40,10 +51,6 @@ class StateCta {
 
 const state = new StateCta(2);
 
-(() => {
-  console.log(state.isOpen());
+state.bindBtn();
 
-  state.bindBtn();
-})();
-
-animatedScrollTo(500);
+// animatedScrollTo(500);
